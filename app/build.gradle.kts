b87d781ebf1af2e76d555f2e2d41b61a51d7bf74
Bhanumahesh Pasham <bhanumaheshpasham@Mac-194.cable.rcn.com>
2025-04-24 22:20:33 -0500

    Dependencies of retrofit, room, firebase were added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services") // Google Services plugin)
 }
 
 android {
@@ -40,4 +41,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.room.runtime)
+    implementation(libs.firebase.auth)
 }
